752f81a326cf8693228a38e1605e2d78b25fdae2
ashaum <ashaum@yahoo.com>
2017-05-15 14:59:54 -0600

    Revert "Update resumeBuilder.js"
    This reverts commit 2e35b5f6c8f4bee06fedb223db99728b8b36b0e9.
    
    # Conflicts:
    #	js/resumeBuilder.js

--- js/resumeBuilder.js
+++ js/resumeBuilder.js
@@ -1,3 +1,8 @@
+<<<<<<< HEAD
 /*
-This is empty on purpose! Your code to build the resume will go here.
+This is empty on purpose! Your code to build the resume will go here. THIS IS A TEST!
  */
+=======
+$("main").append(["Jame II"]);
+
+>>>>>>> parent of 2e35b5f... Update resumeBuilder.js
